79d1898f5ad1d3248c3c4c346a815aed74e7e194
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-15 07:52:35 +0530

    feat: got message from server component

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -76,7 +76,27 @@ export default function VideoMeetComponent()
 
     let gotMessageFromServer = (fromId,message)=>
     {
-      
+      var signal = JSON.parse(message);
+      if(fromId !== socketIdRef.current)
+      {
+        if(signal.sdp)
+        {
+          connections[fromId].setREmoteDescription(new RTCSessionDescription(signal.sdp)).then(()=>{
+            if(signal.sdp.type==="offer")
+            {
+              connections[fromId].createAnswer().then((description)=>{
+                connections[fromId].setLocalDescription(description).then(()=>{
+                    socketIdRef.current.emit("signal",fromId,JSON.stringify({"sdp":connections[fromId].localDescription}))
+                }).catch(e=>console.log(e))
+              }).catch(e=>console.log(e))
+            }
+          }).catch(e=>console.log(e))
+        }
+        if(signal.ice)
+        {
+          connections[fromId].addIceCandidate(new RTCIceCandidate(signal.ice)).catch(e => console.log(e));
+        }
+      }
     }
 
     let addMessage = () => {
