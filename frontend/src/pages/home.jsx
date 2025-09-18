f62f3a87afc49d2d08b1c1a86350f4f6552f2dd7
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-18 22:56:14 +0530

    completed home component

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import withAuth from "../utils/withAuth";
+import { useNavigate} from "react-router-dom";
+import "../App.css";
+import {IconButton, TextField} from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+import { Button } from "@mui/material";
 
 function HomeComponent()
 {
+    let navigate = useNavigate();
+    const [meetingCode,setMeetingCode] = useState("");
+    let handleVideoCall = async()=>
+    {
+        navigate(`/${meetingCode}`);
+    }
     return(
-        <div>HomeComponent</div>
+        <>
+            <div className="navBar">
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <h2>Apna Video Call</h2>
+
+                </div>
+                <div style={{display:"flex",alignItems:"center"}}>
+                    <IconButton>
+                        <RestoreIcon/>
+                        <p>History</p>
+                    </IconButton>
+                    <Button onClick={()=>{
+                        localStorage.removeItem("token")
+                        navigate("/auth");
+                    }}>
+                        Logout
+                    </Button>
+                </div>
+            </div>
+
+            <div className="meetContainer">
+                <div className="leftPanel">
+                    <div>
+                        <h2>Providing Quality Video Call just like Quality Education</h2>
+                        <div style={{display:"flex",gap:"10px"}}>
+                        <TextField onChange={e=>setMeetingCode(e.target.value)} id="outlined-basic" label="Meeting Code" variant="outlined"></TextField>
+                        <Button onClick={handleVideoCall} variant="contained">Join</Button>
+
+                        </div>
+                    </div>
+                </div>
+                <div className="rightPanel">
+                    <img srcSet="/logo4.png" alt=""></img>
+                </div>
+            </div>
+        </>
     )
 }
 
