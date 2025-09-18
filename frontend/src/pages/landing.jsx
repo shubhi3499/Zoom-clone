7fcc5a6caf77d9dfd82c09fb7dc2d24878a7e69a
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-19 00:10:44 +0530

    history component completed

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import "../App.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function LandingPage()
 {
+    const router = useNavigate();
     return(
         <div className='landingPageConatiner'>
             <nav>
@@ -11,9 +13,15 @@ export default function LandingPage()
                     <h2>Apna VideoCall</h2>
                 </div>
                 <div className='navList'>
-                    <p>Join as Guest</p>
-                    <p>Register</p>
-                    <div role='button'>
+                    <p onClick={()=>{
+                        router("/aljk23");
+                    }}>Join as Guest</p>
+                    <p onClick={()=>{
+                        router("/auth")
+                    }}>Register</p>
+                    <div onClick={()=>{
+                        router("/auth")
+                    }}role='button'>
                         <p>Login</p>
                     </div>
                 </div>
